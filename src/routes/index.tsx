472149e902d58ac09209e94faf21c051fb33e296
JavaVista <suavejavi@gmail.com>
2023-05-11 10:58:06 -0400

    add onClick event and component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,17 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <div>
-      Llamas Rules!
-    </div>
+    <>
+      <Display />
+      <div>
+        Hola Mundo!
+      </div>
+      <button onClick$={() => console.log('Hola')}>Heya!</button>
+    </>
+
   );
+});
+
+export const Display = component$(() => {
+  return <div>Hey!</div>
 });
