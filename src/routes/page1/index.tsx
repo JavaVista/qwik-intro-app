5386150339a3bbb9130076e01501d66b579ccb9a
JavaVista <suavejavi@gmail.com>
2023-05-11 11:07:31 -0400

    add event listerner to input

--- src/routes/page1/index.tsx
+++ src/routes/page1/index.tsx
@@ -2,17 +2,19 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
 
-  
+
 
   return <div>
     This is Page 1
 
     <hr />
-    
-    <input type="text" placeholder="Type your search"/>
-    
+
+    <input type="text" placeholder="Type your search" onInput$={(e: InputEvent) => {
+      console.log((e.target as HTMLInputElement).value)
+    }} />
+
     <hr />
-    
+
     <div>You typed: </div>
   </div>
 });
